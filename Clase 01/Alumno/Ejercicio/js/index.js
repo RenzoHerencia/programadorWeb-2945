5b05d6778802239a8ad71923503605a58dc75088
Renzo Herencia <rnzo.ho@gmail.com>
2018-02-17 12:39:33 -0300

    Ejercicio1

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,31 @@
+var nombre = prompt('Ingrese su nombre')
+
+console.log ('Hola', nombre+'!')
+
+var age = prompt('Ingrese su edad y sumará 15')
+
+var parsedAge = parseInt(age, 10)
+
+var number1 = 15
+
+console.log (parsedAge+number1)
+
+var age = prompt('Ingrese su edad y restará 25')
+
+var parsedAge2 = parseInt(age, 10)
+
+var number2 = 25
+
+console.log (parsedAge2-number2)
+
+var number3 = prompt('Ingrese un número')
+
+var number4 = prompt('Ingrese otro número')
+
+var parsedNumber3 = parseInt(number3, 10)
+
+var parsedNumber4 = parseInt(number4, 10)
+
+console.log (parsedNumber3+parsedNumber4)
+
+
