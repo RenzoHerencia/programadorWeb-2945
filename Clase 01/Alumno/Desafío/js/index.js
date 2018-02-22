60329b0fd50b064f03ae411a91b8edd1200be6e0
RenzoHerencia <35631061+RenzoHerencia@users.noreply.github.com>
2018-02-22 14:11:06 -0300

    Desafío 1

---
+++ Clase 01/Alumno/Desafío/js/index.js
@@ -0,0 +1,17 @@
+var number1 = prompt('Ingrese un numero')
+
+var number2 = prompt('Ingrese otro numero')
+
+var parsedNumber1 = parseInt(number1, 10)
+
+var parsedNumber2 = parseInt(number2, 10)
+
+console.log ('La suma es '+(parsedNumber1+parsedNumber2))
+
+console.log ('La resta es '+(parsedNumber1-parsedNumber2))
+
+console.log ('La multiplicación es '+(parsedNumber1*parsedNumber2))
+
+console.log ('La división es '+(parsedNumber1/parsedNumber2))
+
+
